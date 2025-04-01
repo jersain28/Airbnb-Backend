2556f9984fe594eed4e49bbb58764d9b331207b4
unknown <jersainmarin20679@gmail.com>
2025-03-31 20:19:37 -0600

    17 Make it possible to connection

--- djangobnb_backend/chat/serializers.py
+++ djangobnb_backend/chat/serializers.py
@@ -10,3 +10,10 @@ class ConversationListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ('id', 'users', 'created_at', 'modified_at',)
+
+class ConversationDetailSerializer(serializers.ModelSerializer):
+     users = UserDetailSerializer(many=True, read_only=True)
+
+     class Meta:
+        model = Conversation
+        fields = ('id', 'users', 'created_at', 'modified_at',)
